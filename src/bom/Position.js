ce6924722680522e2659ba94adba72fad4cc8c1a
Sebastian Werner <swerner@zynga.com>
2011-11-30 17:42:38 +0100

    Added assertions

--- src/bom/Position.js
+++ src/bom/Position.js
@@ -13,6 +13,7 @@
 		var gpuStacking = !!core.bom.Style.property('perspective');
 		if (!gpuStacking) {
 
+			// Note: translateZ is supported on some clients even if 3D transforms and perspective is not.
 			var elem = document.createElement("div");
 			var style = elem.style;
 			var value = "translateZ(20px)";
@@ -23,9 +24,36 @@
 		}
 	}
 	
-	if (gpuStacking) {
-		
-		var set = function(elem, x, y, z, zoom) {
+	// Verify incoming parameters
+	if (core.Env.isSet("debug")) 
+	{
+		var validate = function(args) 
+		{
+			// Three required parameters
+			core.Test.assertElement(args[0]);
+			core.Test.assertNumber(args[1]);
+			core.Test.assertNumber(args[2]);
+			
+			// Two optional ones
+			if (args[3] != null) 
+			{
+				// zIndex does not support floats.
+				core.Test.assertInteger(args[3]);
+			}
+
+			if (args[4] != null) {
+				core.Test.assertNumber(args[4]);
+			}
+		};
+	}
+	
+	if (gpuStacking) 
+	{
+		var set = function(elem, x, y, z, zoom) 
+		{
+			if (core.Env.isSet("debug")) {
+				validate(arguments);
+			}
 
 			// We default to z position zero instead of leaving it untouched to force acceleration in Safari
 			if (z == null) {
@@ -41,15 +69,22 @@
 			elem.style[transform] = value;
 		};
 		
-		var reset = function(elem) {
+		var reset = function(elem) 
+		{
+			if (core.Env.isSet("debug")) {
+				core.Test.assertElement(elem);
+			}
 			
 			elem.style[transform] = "";
-			
 		};
-
-	} else if (transform) {
-		
-		var set = function(elem, x, y, z, zoom) {
+	} 
+	else if (transform) 
+	{
+		var set = function(elem, x, y, z, zoom) 
+		{
+			if (core.Env.isSet("debug")) {
+				validate(arguments);
+			}
 			
 			var value = "translate(" + x + "px," + y + "px)";
 
@@ -65,17 +100,24 @@
 			}
 		};
 
-		var reset = function(elem) {
+		var reset = function(elem) 
+		{
+			if (core.Env.isSet("debug")) {
+				core.Test.assertElement(elem);
+			}
 			
 			var style = elem.style;
 			style[transform] = "";
 			style.zIndex = "";
-			
 		};
-		
-	} else {
-		
-		var set = function(elem, x, y, z, zoom) {
+	}
+	else 
+	{
+		var set = function(elem, x, y, z, zoom) 
+		{
+			if (core.Env.isSet("debug")) {
+				validate(arguments);
+			}
 			
 			var style = elem.style;
 			style.left = (x / zoom) + "px";
@@ -90,29 +132,31 @@
 			}
 		};
 
-		var reset = function(elem) {
+		var reset = function(elem) 
+		{
+			if (core.Env.isSet("debug")) {
+				core.Test.assertElement(elem);
+			}
 			
 			var style = elem.style;
 			style.left = style.top = style.zIndex = style.zoom = "";
-
 		};
-
 	}
 	
 	
 	/**
 	 * High performance DOM node positioning with stacking and zooming support
 	 */
-	core.Module("core.bom.Position", {
-		
+	core.Module("core.bom.Position", 
+	{
 		/**
 		 * Positions the given DOM element on coordinates x, y and z.
 		 *
 		 * @param elem {Element} DOM element to position
 		 * @param x {Number} Left position
 		 * @param y {Number} Top position
-		 * @param z {Integer} Depth position
-		 * @param zoom {Number} Zooming to apply
+		 * @param z {Integer ? null} Depth position
+		 * @param zoom {Number ? null} Zooming to apply
 		 */ 
 		set: set,
 		
@@ -122,7 +166,5 @@
 		 * @param elem {Element} DOM element to reset
 		 */
 		reset: reset
-		
 	});
-	
 })();
