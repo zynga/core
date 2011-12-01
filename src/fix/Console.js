1b901579ba36ff60d23f2e186987f1388ad13242
Sebastian Werner <swerner@zynga.com>
2011-12-01 16:14:15 +0100

    Reduced number of emulated methods. Better assert() emulation.

--- src/fix/Console.js
+++ src/fix/Console.js
@@ -7,10 +7,13 @@
 
 /**
  * Just a dump placeholder for environments without "console" object.
+ *
+ * Emulates just the basic logging methods with an empty function. Maps missing functions
+ * to `console.log` automatically.
  */
 (function(global)
 {
-	var methods = "log,clear,assert,debug,error,warn,info,trace".split(",");
+	var methods = "log,debug,error,warn,info".split(",");
 	var console = global.console || (global.console = {});
 	var log = console.log || new Function;
 
@@ -19,6 +22,16 @@
 		var name = methods[i];
 		if (!console[name]) {
 			console[name] = log;
+		}
+	}
+	
+	if (!console.assert) 
+	{
+		console.assert = function(expression) 
+		{
+			if (!expression) {
+				throw new Error(Array.prototype.slice.call(arguments, 1).join(" "));
+			}
 		}
 	}
 })(this);
