2c92697654510649f08038ee5ba6e067aa24b627
Sebastian Werner <swerner@zynga.com>
2012-01-26 16:30:17 +0100

    Restructured

--- test/tests.js
+++ test/tests.js
@@ -343,12 +343,12 @@ $(function() {
 	
 	test("Text", function() {
 		
-		strictEqual(typeof core.util.Text.measure("hello world"), "object");
-		strictEqual(typeof core.util.Text.measure("hello world").width, "number");
-		strictEqual(typeof core.util.Text.measure("hello world").height, "number");
+		strictEqual(typeof core.bom.Text.measure("hello world"), "object");
+		strictEqual(typeof core.bom.Text.measure("hello world").width, "number");
+		strictEqual(typeof core.bom.Text.measure("hello world").height, "number");
 
-		strictEqual(typeof core.util.Text.measure("hello world foo bar baz", null, 40), "object");
-		strictEqual(core.util.Text.measure("hello world foo bar baz", null, 40).width, 40);
+		strictEqual(typeof core.bom.Text.measure("hello world foo bar baz", null, 40), "object");
+		strictEqual(core.bom.Text.measure("hello world foo bar baz", null, 40).width, 40);
 		
 	});
 	
