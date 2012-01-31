0c9b675740852d1298e688fbb564f1033c8ab80d
Sebastian Werner <swerner@zynga.com>
2012-01-31 14:06:10 +0100

    API docs

--- src/locale/Translate.js
+++ src/locale/Translate.js
@@ -10,42 +10,10 @@
 	// Jasy is replacing this call via the kernel permutation
 	var translations = core.Env.getValue("translations");
 	
-	/**
-	 *
-	 *
-	 */
-	core.Module("core.locale.Translate",
-	{
-		/**
-		 * Quick and easy string templating using %1, %2, etc. as placeholders
-		 * and an array for the data.
-		 *
-		 * @param msg {String} Input string with placeholders
-		 * @param data {Array} List of values
-		 * @param start {Integer?-1} Where to start in the array. Defaults to -1 which is
-		 *	 useful when the template string should map %1 = data[0]
-		 * @return {String} Result string
-		 */
-		template : function(msg, data, start)
-		{
-			// %1 = first, %2 = second, ...
-			start = start == null ? -1 : start - 1;
-			return msg.replace(/%([0-9])/g, function(match, pos) {
-				return data[start+parseInt(pos)];
-			});
-		},
-
-
-		/**
-		 * Applies the plural rule of the current locale and returns the
-		 * field index on the translation data. This is the data used
-		 * by the classical GNU gettext tools. See also:
-		 * http://www.gnu.org/software/hello/manual/gettext/Plural-forms.html
-		 *
-		 * @param n {Number} Number to test
-		 * @return {String} One of 0, 1, 2, 3, 4, 5 or 6
-		 */
-		plural : global.locale && (function(fields, Plural)
+	var plural;
+	if (global.locale) {
+		
+		plural = (function(fields, Plural)
 		{
 			var code="", pos=0;
 			var field, expr;
@@ -61,20 +29,57 @@
 			code += "return " + pos + ";"
 
 			return new Function("n", code);
-		})(["ZERO", "ONE", "TWO", "FEW", "MANY"], locale.Plural),
+		})(["ZERO", "ONE", "TWO", "FEW", "MANY"], locale.Plural);
+		
+	}
+	else 
+	{
+		/**
+		 * {Integer} Applies the plural rule to the given @n {Number} of the current locale and returns the
+		 * field index on the translation data (0-6). This is the data used
+		 * by the classical GNU gettext tools. See also:
+		 * http://www.gnu.org/software/hello/manual/gettext/Plural-forms.html
+		 */
+		plural = function(n) {
+			return 0;
+		}
+	}
+	
+	/**
+	 * {String} Quick and easy string templating using %1, %2, etc. as placeholders
+	 * and an array for the data. You pass in the @message {String} and the @data {Array}
+	 * and an optional @start {Integer?-1} to configure the initial array position to use.
+	 */
+	var template = function(message, data, start)
+	{
+		// %1 = first, %2 = second, ...
+		start = start == null ? -1 : start - 1;
+		return message.replace(/%([0-9])/g, function(match, pos) {
+			return data[start+parseInt(pos)];
+		});
+	};
+	
+	
+	/**
+	 * Developer interface to translation API of Core Library.
+	 */
+	core.Module("core.locale.Translate",
+	{
+		template : template,
+		plural : plural,
 
 
 		/**
 		 * Translates the given message and replaces placeholders in the result string.
 		 *
-		 * @param msg {String} Message to translate (used as a fallback when no translation is available)
-		 * @param varargs {...} Placeholder values
+		 * @param message {String} Message to translate (used as a fallback when no translation is available)
+		 * @param varargs {var...} Placeholder values
 		 * @return {String} Translated string
 		 */
-		tr : function(msg, varargs)
+		tr : function(message, varargs)
 		{
-			var replacement = translations[msg] || msg;
-			return arguments.length <= 1 ? replacement : core.locale.Translate.template(replacement, arguments, 1);
+			var replacement = translations[message] || message;
+			return arguments.length <= 1 ? replacement : template(replacement, arguments, 1);
 		},
 
 
@@ -83,42 +88,42 @@
 		 * replaces placeholders in the result string.
 		 *
 		 * @param hint {String} Hint for the translator of the message
-		 * @param msg {String} Message to translate (used as a fallback when no translation is available)
-		 * @param varargs {...} Placeholder values
+		 * @param message {String} Message to translate (used as a fallback when no translation is available)
+		 * @param varargs {var...} Placeholder values
 		 * @return {String} Translated string
 		 */
-		trc : function(hint, msg, varargs)
+		trc : function(hint, message, varargs)
 		{
-			var replacement = translations[msg] || msg;
-			return arguments.length <= 2 ? replacement : core.locale.Translate.template(replacement, arguments, 2);
+			var replacement = translations[message] || message;
+			return arguments.length <= 2 ? replacement : template(replacement, arguments, 2);
 		},
 
 
 		/**
 		 * Translates the given message and replaces placeholders in the result string.
 		 *
-		 * @param msgSingular {String} Fallback message for singular case
-		 * @param msgPlural {String} Fallback message for plural case
+		 * @param messageSingular {String} Fallback message for singular case
+		 * @param messagePlural {String} Fallback message for plural case
 		 * @param number {Integer} Number of items (chooses between the exact translation which is being used)
-		 * @param varargs {...} Placeholder values
+		 * @param varargs {var...} Placeholder values
 		 * @return {String} Translated string
 		 */
-		trn : global.locale && function(msgSingular, msgPlural, number, varargs)
+		trn : global.locale && function(messageSingular, messagePlural, number, varargs)
 		{
-			// Matching is based on singular "msgid"
-			var replacement = translations[msgSingular];
+			// Matching is based on singular "messageid"
+			var replacement = translations[messageSingular];
 
 			// Do numeric lookup
 			if (typeof replacement == "object") {
-				var result = replacement[core.locale.Translate.plural(number)];
+				var result = replacement[plural(number)];
 			}
 
 			// Fallback to programmatically defined messages
 			if (!result) {
-				result = number == 1 ? msgSingular : msgPlural;
+				result = number == 1 ? messageSingular : messagePlural;
 			}
 
-			return arguments.length <= 3 ? result : core.locale.Translate.template(result, arguments, 3);
+			return arguments.length <= 3 ? result : template(result, arguments, 3);
 		}
 	});
 })(this);
