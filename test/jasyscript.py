da5eaf4cc2baea384fb3012e7e52cb0e39f2cf14
Sebastian Werner <swerner@zynga.com>
2012-02-01 14:24:07 +0100

    Jasyified unit tests

---
+++ test/jasyscript.py
@@ -0,0 +1,13 @@
+
+@task("Writing Test Loader")
+def test():
+    session = Session()
+    session.addProject(Project("../"))
+    session.addProject(Project("."))
+    
+    resolver = Resolver(session.getProjects())
+    resolver.addClassName("tests")
+    classes = Sorter(resolver).getSortedClasses()
+
+    storeCompressed("build.js", classes, formatting=Formatting("semicolon", "comma"))
+    
