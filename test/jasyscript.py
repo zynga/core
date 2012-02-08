a259b9d587011eb9548cb7e27efe2898f6288c5d
Sebastian Werner <swerner@zynga.com>
2012-02-08 16:35:24 +0100

    Worked on test refactoring

--- test/jasyscript.py
+++ test/jasyscript.py
@@ -1,18 +1,80 @@
 
+@task("Source")
+def source():
+    session = Session()
+    session.addProject(Project("../"))
+    session.addProject(Project("."))
+    
+    # Configure fields
+    session.permutateField("es5")
+    session.permutateField("debug")
+
+    # Include all game relevant assets
+    resolver = Resolver(session.getProjects())
+    resolver.addClassName("tests")
+    assets = Asset(session, resolver.getIncludedClasses()).exportSource()
+
+    # Store kernel script
+    includedByKernel = storeKernel("source/script/kernel.js", session, assets=assets)
+    
+    # Process every possible permutation
+    for permutation in session.getPermutations():
+
+        # Resolving dependencies
+        resolver = Resolver(session.getProjects())
+        resolver.addClassName("tests")
+        resolver.excludeClasses(includedByKernel)
+        
+        # Writing source loader
+        destination = "source/script/test-%s.js" % permutation.getChecksum()
+        classes = Sorter(resolver).getSortedClasses()
+        storeSourceLoader(destination, classes, session)
+        
+    session.close()
+    
+
+
 @task("Build")
 def build():
     session = Session()
     session.addProject(Project("../"))
     session.addProject(Project("."))
-    
+
+    # Configure permutations
+    session.permutateField("es5")
+    session.permutateField("debug")
+
+    # Prepare assets
     resolver = Resolver(session.getProjects())
-    resolver.addClassName("jquery")
-    resolver.addClassName("qunit")
     resolver.addClassName("tests")
-    classes = Sorter(resolver).getSortedClasses()
-    print(classes)
-    
-    storeCompressed("build.js", classes, formatting=Formatting("semicolon", "comma"))
+    assets = Asset(session, resolver.getIncludedClasses()).exportBuild()
+
+    # Write kernel script
+    includedByKernel = storeKernel("build/script/kernel.js", session, assets=assets)
+
+    # Copy files from source
+    for staticFile in ["index.html"]:
+        updateFile("source/%s" % staticFile, "build/%s" % staticFile)
+
+    # Compiler configuration
+    optimization = Optimization("variables", "declarations", "blocks")
+    formatting = Formatting()
+
+    # Process every possible permutation
+    for permutation in session.getPermutations():
+
+        # Resolving dependencies
+        resolver = Resolver(session.getProjects(), permutation)
+        resolver.addClassName("tests")
+        resolver.excludeClasses(includedByKernel)
+
+        # Compressing classes
+        classes = Sorter(resolver, permutation).getSortedClasses()
+        compressedCode = storeCompressed("build/script/test-%s.js" % permutation.getChecksum(), classes, 
+            permutation=permutation, optimization=optimization, formatting=formatting)
+
+    session.close()
+
     
     
     
--- test/src/tests.js
+++ test/src/tests.js
@@ -1,3 +1,5 @@
+/** #require(jquery) #require(qunit) */
+
 var global = this;
 $(function() {
 	
