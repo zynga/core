5777b86ab8eeec6d2fffd4edf908ad2c715d0fa0
Sebastian Werner <swerner@zynga.com>
2012-02-08 17:30:34 +0100

    Cleanup of jasyscript

--- test/jasyscript.py
+++ test/jasyscript.py
@@ -1,23 +1,25 @@
+session = Session()
+session.addProject(Project("../"))
+session.addProject(Project("."))
+
+# Configure permutations
+session.permutateField("es5")
+session.setField("debug", True)
+
+# Compiler configuration
+optimization = Optimization("variables", "declarations", "blocks")
+formatting = Formatting("semicolon", "comma")
+
 
 @task("Source")
 def source():
-    session = Session()
-    session.addProject(Project("../"))
-    session.addProject(Project("."))
-    
-    # Configure fields
-    session.permutateField("es5")
-    session.setField("debug", True)
-
     # Include all game relevant assets
     resolver = Resolver(session.getProjects())
     resolver.addClassName("tests")
     assets = Asset(session, resolver.getIncludedClasses()).exportSource()
 
     # Store kernel script
-    formatting = Formatting("semicolon", "comma")
     includedByKernel = storeKernel("source/script/kernel.js", session, assets=assets, debug=True, formatting=formatting)
-    print("KERNEL: %s" % includedByKernel)
     
     # Process every possible permutation
     for permutation in session.getPermutations():
@@ -31,29 +33,15 @@ def source():
         destination = "source/script/test-%s.js" % permutation.getChecksum()
         classes = Sorter(resolver).getSortedClasses()
         storeSourceLoader(destination, classes, session)
-        
-    session.close()
     
 
 
 @task("Build")
 def build():
-    session = Session()
-    session.addProject(Project("../"))
-    session.addProject(Project("."))
-
-    # Configure permutations
-    session.permutateField("es5")
-    session.setField("debug", True)
-
     # Prepare assets
     resolver = Resolver(session.getProjects())
     resolver.addClassName("tests")
     assets = Asset(session, resolver.getIncludedClasses()).exportBuild()
-
-    # Compiler configuration
-    optimization = Optimization("variables", "declarations", "blocks")
-    formatting = Formatting("semicolon", "comma")
 
     # Write kernel script
     includedByKernel = storeKernel("build/script/kernel.js", session, assets=assets, debug=True, formatting=formatting)
@@ -75,15 +63,9 @@ def build():
         compressedCode = storeCompressed("build/script/test-%s.js" % permutation.getChecksum(), classes, 
             permutation=permutation, optimization=optimization, formatting=formatting)
 
-    session.close()
-
     
     
     
 @task("Clear Cache")
 def clear():
-    session = Session()
-    session.addProject(Project("../"))
-    session.addProject(Project("."))
     session.clearCache()
-    session.close()    
