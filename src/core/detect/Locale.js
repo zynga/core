f8c746617715e7d14ce75bd30e9b5950f9716f18
Sebastian Werner <swerner@zynga.com>
2012-02-14 14:04:35 +0100

    Imporved support for non browsers

--- src/core/detect/Locale.js
+++ src/core/detect/Locale.js
@@ -13,12 +13,12 @@ core.Module("core.detect.Locale",
 	/**
 	 * {=String} Locale string as configured by the user e.g. `de_AT`, `es_ES` or short like `de`
 	 */
-	VALUE : (function()
+	VALUE : (function(global)
 	{
-		var nav = navigator;
-		var input = (nav.userLanguage || nav.language).toLowerCase();
+		var nav = global.navigator || {};
+		var input = (nav.userLanguage || nav.language || "en").toLowerCase();
 		var split = input.indexOf("-");
 
 		return split > 0 ? input.substring(0, split) : input;
-	})()
+	})(this)
 });
