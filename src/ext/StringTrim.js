e61525b18379e033094302c8148c432209e3eaef
Sebastian Werner <info@sebastian-werner.net>
2012-02-27 18:57:51 +0100

    Minor changes

--- src/ext/StringTrim.js
+++ src/ext/StringTrim.js
@@ -18,9 +18,9 @@
 	/**
 	 * Adds ES5 String methods if these are not implemented by the engine.
 	 *
-	 * - trim() (ES5)
-	 * - trimLeft() (JavaScript 1.8.1)
-	 * - trimRight() (JavaScript 1.8.1)
+	 * - `trim()` (ES5)
+	 * - `trimLeft()` (JavaScript 1.8.1)
+	 * - `trimRight()` (JavaScript 1.8.1)
 	 */
 	core.Main.addMembers("String", 
 	{
