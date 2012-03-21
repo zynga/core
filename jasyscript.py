074af5d156127dc15d327f46b58cd841ed14eec8
Sebastian Werner <swerner@zynga.com>
2012-03-21 16:16:25 +0100

    Minor changes

--- jasyscript.py
+++ jasyscript.py
@@ -17,27 +17,27 @@ def distclean():
     removeDir("dist")
 
 
-@task("Build API Viewer")
+@task("Build API browser")
 def api():
     ApiWriter().write("data")
-    runTask("api", "build")
+    runTask("apibrowser", "build")
 
 
-@task("Write module.js", prefix="dist")
+@task("Build module.js", prefix="dist")
 def module():
     for permutation in session.permutate():
         resolver = Resolver().addClassName("core.Module")
         storeCompressed("module-%s.js" % permutation.getChecksum(), Sorter(resolver).getSortedClasses())
 
 
-@task("Write oo.js", prefix="dist")
+@task("Build oo.js", prefix="dist")
 def oo():
     for permutation in session.permutate():
         resolver = Resolver().addClassName("core.Module").addClassName("core.Class")
         storeCompressed("oo-%s.js" % permutation.getChecksum(), Sorter(resolver).getSortedClasses())
         
         
-@task("Write sugar.js", prefix="dist")
+@task("Build sugar.js", prefix="dist")
 def sugar():
     for permutation in session.permutate():
         resolver = Resolver()
