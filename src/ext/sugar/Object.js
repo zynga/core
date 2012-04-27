b0b0d092506830aaeeed6275339a31e2f1efe76c
Sebastian Werner <swerner@zynga.com>
2012-04-27 12:53:41 +0200

    Improved isEmpty. Added clone and pick.

--- src/ext/sugar/Object.js
+++ src/ext/sugar/Object.js
@@ -5,81 +5,124 @@
 ==================================================================================================
 */
 
-/**
- * Adds useful non-standard extensions to the Object class like {#fromArray} and {#values}.
- */
-core.Main.addStatics("Object", 
-{
-	/**
-	 * {Map} Creates a new object with prefilled content from the @keys {Array} list.
-	 * The @value {var ? true} is always the same, defaults to true, but is also configurable.
-	 */
-	fromArray : function(keys, value) 
-	{
-		if (arguments.length == 1) {
-			value = true;
-		}
-		
-		var obj = {};
-		for (var i=0, l=keys.length; i<l; i++) {
-			obj[keys[i]] = value;
-		}
-		
-		return obj;
-	},
+(function() {
 	
-
-	/**
-	 * {Boolean} Tests whether the given @object {Object} is empty
-	 */
-	isEmpty: function(object) 
-	{
-		for (var key in object) {
-			return false;
-		}
-		
-		// Another check required for buggy browsers (wrong enum handling)
-		return Object.keys(object).length == 0;
-	},
-	
-	
-	/**
-	 * {Array} Returns all the values of the given @object {Map}.
-	 */
-	values : function(object) 
-	{
-		return Object.keys(object).map(function(key) {
-			return object[key];
-		});
-	},
-	
-	
-	/**
-	 * {String} Validates the @object {Map} to don't hold other keys than the ones defined by @allowed {Array}. 
-	 * Returns first non matching key which was found or `undefined` if all keys are valid.
-	 */
-	validateKeys : function(object, allowed) 
-	{
-		// Build lookup table
-		var set = {};
-		for (var i=0, l=allowed.length; i<l; i++) {
-			set[allowed[i]] = true;
-		}
-		
-		// Collect used keys
-		var list = Object.keys(object);
-		
-		// Validate keys
-		var invalid = [];
-		for (var i=0, l=list.length; i<l; i++) 
-		{
-			var current = list[i];
-			if (!set[current]) {
-				invalid.push(current);
-			}
-		}
-		
-		return invalid;
+	// Fix for IE bug with enumerables
+	var hasDontEnumBug = true;
+	for (var key in {"toString": null}) {
+		hasDontEnumBug = false;	
 	}
-});
+	
+	/**
+	 * Adds useful non-standard extensions to the Object class like {#fromArray} and {#values}.
+	 */
+	core.Main.addStatics("Object", 
+	{
+		/**
+		 * {Map} Creates a new object with prefilled content from the @keys {Array} list.
+		 * The @value {var ? true} is always the same, defaults to true, but is also configurable.
+		 */
+		fromArray : function(keys, value) 
+		{
+			if (arguments.length == 1) {
+				value = true;
+			}
+
+			var obj = {};
+			for (var i=0, l=keys.length; i<l; i++) {
+				obj[keys[i]] = value;
+			}
+
+			return obj;
+		},
+
+
+		/**
+		 * {Boolean} Tests whether the given @object {Object} is empty
+		 */
+		isEmpty: function(object) 
+		{
+			for (var key in object) {
+				return false;
+			}
+			
+			// Another check required for buggy browsers (wrong enum handling)
+			return !hasDontEnumBug || Object.keys(object).length == 0;
+		},
+
+
+		/**
+		 * {Array} Returns all the values of the given @object {Map}.
+		 */
+		values : function(object) 
+		{
+			return Object.keys(object).map(function(key) {
+				return object[key];
+			});
+		},
+
+
+		/**
+		 * {Map} Create a shallow-copied clone of the @object {Map}. Any nested 
+		 * objects or arrays will be copied by reference, not duplicated.
+		 */
+		clone : function(object) 
+		{
+			var result = {};
+			for (var key in object) {
+				result[key] = object[key];
+			}
+			return result;
+		},
+
+
+		/**
+		 * {Map} Returns a copy of the @object {Map}, filtered to only have values for the whitelisted @keys {String...}.
+		 */
+		pick : function(object, keys) 
+		{
+			var result = {};
+			var args = arguments;
+
+			for (var i=1, l=args.length; i<l; i++) 
+			{
+				var key = args[i];
+				result[key] = object[key];
+			}
+
+			return result;
+		},
+
+
+		/**
+		 * {String} Validates the @object {Map} to don't hold other keys than the ones defined by @allowed {Array}. 
+		 * Returns first non matching key which was found or `undefined` if all keys are valid.
+		 */
+		validateKeys : function(object, allowed) 
+		{
+			// Build lookup table
+			var set = {};
+			for (var i=0, l=allowed.length; i<l; i++) {
+				set[allowed[i]] = true;
+			}
+
+			// Collect used keys
+			var list = Object.keys(object);
+
+			// Validate keys
+			var invalid = [];
+			for (var i=0, l=list.length; i<l; i++) 
+			{
+				var current = list[i];
+				if (!set[current]) {
+					invalid.push(current);
+				}
+			}
+
+			return invalid;
+		}
+	});
+	
+})();
+
 
