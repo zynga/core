30921efe47146fc61a5574fc6ae969cb1a3b1d14
Sebastian Werner <swerner@zynga.com>
2012-06-04 17:30:19 +0200

    Fixed for Jasy 0.7 API

--- jasyscript.py
+++ jasyscript.py
@@ -21,14 +21,14 @@ def distclean():
 def module():
     for permutation in session.permutate():
         resolver = Resolver().addClassName("core.Module")
-        storeCompressed(resolver, "module-%s.js" % permutation.getChecksum())
+        storeCompressed(resolver.getSortedClasses(), "module-%s.js" % permutation.getChecksum())
 
 
 @task("Build oo.js", prefix="dist")
 def oo():
     for permutation in session.permutate():
         resolver = Resolver().addClassName("core.Module").addClassName("core.Class")
-        storeCompressed(resolver, "oo-%s.js" % permutation.getChecksum())
+        storeCompressed(resolver.getSortedClasses(), "oo-%s.js" % permutation.getChecksum())
         
         
 @task("Build sugar.js", prefix="dist")
@@ -41,6 +41,6 @@ def sugar():
         resolver.addClassName("ext.sugar.Object")
         resolver.addClassName("ext.sugar.String")
 
-        storeCompressed(resolver, "sugar-%s.js" % permutation.getChecksum())
+        storeCompressed(resolver.getSortedClasses(), "sugar-%s.js" % permutation.getChecksum())
 
                 
