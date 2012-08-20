d9e5929ea10fa6d34b2392c4c56b828d04578b1f
Sebastian Werner <swerner@zynga.com>
2012-08-20 16:33:44 +0200

    Updated test

--- skeleton/application/jasycreate.py
+++ skeleton/application/jasycreate.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-config.ask("What's your name", "name", "String")
+# This script is executed by Jasy during creating new applications
+
+config.ask("What's your name", "name", "String", defaultValue="Default User")
 config.ask("How old are you", "age", "Integer")
-config.ask("What's PI", "pi", "Float")
+config.ask("What's PI", "pi", "Float", defaultValue=3.14)
+config.ask("Increment from 1..3", "incr", "List")
 
 config.save("custom", [1,2,3])
