750c6ad54b23e7edc21eb6cc66bde4989d8759d2
Sebastian Werner <swerner@zynga.com>
2012-08-21 10:23:21 +0200

    Test for nested structures

--- skeleton/application/jasycreate.py
+++ skeleton/application/jasycreate.py
@@ -2,8 +2,8 @@
 
 # This script is executed by Jasy during creating new applications
 
-config.ask("What's your name", "name", "String", defaultValue="Default User")
-config.ask("How old are you", "age", "Integer")
+config.ask("What's your name", "user.name", "String", defaultValue="Default User")
+config.ask("How old are you", "user.age", "Integer")
 config.ask("What's PI", "pi", "Float", defaultValue=3.14)
 config.ask("Increment from 1..3", "incr", "List")
 
