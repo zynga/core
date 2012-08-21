406950bc6eb6f1aaf80b95c79e9141a0b36621d4
Sebastian Werner <swerner@zynga.com>
2012-08-21 16:10:59 +0200

    Added new test

--- skeleton/application/jasycreate.py
+++ skeleton/application/jasycreate.py
@@ -2,9 +2,12 @@
 
 # This script is executed by Jasy during creating new applications
 
+# Test for custom questions
+config.set("custom", [1,2,3])
 # config.ask("What's your name", "user.name", "String")
 # config.ask("How old are you", "user.age", "Integer")
 # config.ask("What's PI", "pi", "Float", default=3.14, required=False)
 # config.ask("Increment from 1..3", "incr", "List")
 
-config.set("custom", [1,2,3])
+# Test for io features
+# io.mv("jasyscript.py", "foobarbaz.py")
